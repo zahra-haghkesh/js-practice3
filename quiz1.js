dceff1eaa31e80de4a75a8e74506949d93faabeb
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-02 18:56:58 +0330

    quiz1

---
+++ quiz1.js
@@ -0,0 +1,5 @@
+//1) Write a code to extract a specified number of characters from a string.
+// example: Robin Sing => extract 4 characters : Robi
+// example: roni kolman => extract 4 characters : roni
+let name = "bahador hashemi";
+console.log(name.slice(0,4));
