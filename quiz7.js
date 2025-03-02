65064326d60e61b43d48e0e633f0c3b43d3992f8
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-03 00:32:37 +0330

    quiz7

---
+++ quiz7.js
@@ -0,0 +1,6 @@
+//7) Convert all 'Hi' in the text below to 'Hello'.:
+// ● "Hi World. Hi everyone. Hi you. Hi be to yar ghadimi, mnm hamon havadar ghadimi."
+
+let hi =
+  "Hi World. Hi everyone. Hi you. Hi be to yar ghadimi, mnm hamon havadar ghadimi.";
+console.log(hi.replaceAll("Hi","Hello"));
