1f9cacc419d5cd3fd9bbbe2b1a4fd26f67ac05b0
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-03 00:12:05 +0330

    quiz5

---
+++ quiz5.js
@@ -0,0 +1,5 @@
+//5) Write a code to truncate the given string and put ( … ) after 30 characters:
+// ● We are using JavaScript in APS for learning web development and it is one of the most popular languages among programmers
+let ro5 =
+  "We are using JavaScript in APS for learning web development and it is one of the most popular languages among programmers";
+console.log(ro5.slice(0,30)+ "...");
