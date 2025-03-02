6e4b06b3928057331f55933492d1f16cc0d12141
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-03 00:35:18 +0330

    quiz6

---
+++ quiz6.js
@@ -0,0 +1,4 @@
+//6) Write a code to check if a specific word exists in a string.
+let lorem =
+  "Dolor, fugit inventore ex, totam iste, possimus culpa ad ullam explicabo consequuntur quia";
+console.log(lorem.indexOf("totam"));
