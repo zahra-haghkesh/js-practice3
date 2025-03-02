563437853708e61e140d19df0472f17451199e92
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-03 01:04:55 +0330

    quiz9

---
+++ quiz9.js
@@ -0,0 +1,19 @@
+/* 
+  You are given an statement
+
+  You should remove the word "not" by using .substring() and log the result.
+
+  HINT: You will need to use .substring() twice
+*/
+
+let statement = "I do not like programming";
+
+let result = statement.substring(0,4)+ " " +statement.substring(9);
+
+console.log(result);
+
+/* EXPECTED OUTPUT 
+
+  "I do like programming"
+  
+*/
