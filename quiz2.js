db8d06ec3fc3ef5d6c7bf176a42c0398a739eeb5
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-02 19:05:26 +0330

    quiz2

---
+++ quiz2.js
@@ -0,0 +1,6 @@
+//2) Write a code to convert a string into abbreviated form.
+// example: John Fitzgerald Kennedy  => Fitzgerald
+// example: Henry Bence Jones => Bence
+// example: mohammad ben salman => ben
+let name = "sara hemati asl"
+console.log(name.slice(5,11));
