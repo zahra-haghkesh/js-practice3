22a97fbbb4ba18c41e4cd1c40f30f9a7f836e3fe
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-07 21:19:08 +0330

    quiz7

---
+++ quiz7.js
@@ -0,0 +1,8 @@
+//========================?Q-7==================================
+//? Answer these questions about alert and propmpt
+//todo-1:Alert window should display text "I am an alert!"
+//todo-2:Show a prompt that asks your name and show the result in the alert box
+//todo-3:Show a prompt with the question "what is the sum of 30+50?" with a default value
+alert ("I am an alert!")
+let userName =prompt("what is youre name?")
+let question = prompt( "what is the sum of 30+50?")
