8753bdfe7b87a5da75afce219d63e52ab8f43741
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-07 21:04:58 +0330

    quiz6

---
+++ quiz6.js
@@ -0,0 +1,8 @@
+//========================?Q-6==================================
+//? write an if statement that types true if a variable is divisible by 100, otherwise console log false value
+let number = 20
+if (number /100){
+console.log("true");
+} else {
+    console.log("false");
+}
