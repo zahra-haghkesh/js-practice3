fdd13715b31e90f209fbd46a2d3799f34ed7622c
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-07 22:34:48 +0330

    quiz9

---
+++ quiz9.js
@@ -0,0 +1,30 @@
+//========================?Q-9==================================
+
+/** @format */
+
+// using if, else if, else write code for below conditions:
+/*
+    if Shaden is a mentor print out "Hi Shaden, you are a mentor"
+    if Shaden is a student print out "Hi Shaden, you are student"
+    if Shaden is a teacher print out "Hi Shaden, you are a teacher"
+    if she is none of above, print out "Hi Shaden, you should join APS"
+*/
+
+let yourName = "Shaden";
+let yourRole = "mentor";
+
+if (yourRole="mentor"){
+    console.log("Hi Shaden, you are a mentor");
+}else if(yourRole = "student"){
+    console.log("Hi Shaden, you are student");
+}else if (yourRole = "teacher"){
+    console.log("Hi Shaden, you are a teacher");
+}else {
+    console.log( "Hi Shaden, you should join APS");
+}
+
+/* 
+EXPECTED RESULT
+---------------
+Hi Shaden, you are a mentor.
+*/
