e8c6f7f6a89d41b879ce54272fc0489c5e8d43f9
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-07 18:41:43 +0330

    quiz1

---
+++ quiz1.js
@@ -0,0 +1,22 @@
+//========================?Q-1==================================
+//Answer these questions about comparison
+//todo-1:Write a console log to check whether a number is greater than the other and get the output.
+//todo-2:define three numbers then in the console log check the second one is equal or greater than the first.
+//todo-2-1:Then check the third one is equal or greater than the first.
+
+//todo-3:In the console.log() statement below we use the Greater Than Or Equal operator to check whether the value of numOne is greater than or equal the value of numTwo. It also checks whether the value of numTwo is greater than or equal the value of numThree. Change the code so that both expressions in the console.log() statement logs true.
+
+const numOne = 3;
+const numTwo = 4;
+const numThree = 2;
+
+console.log(numOne >= numTwo, numTwo >= numThree);
+console.log(numOne <= numTwo, numTwo >= numThree);  //my answer
+
+//todo-4:In the console.log() statement below we use the Less Than Or Equal operator to check whether the value of numberOne is less than or equal the value of numberTwo. It also checks whether the value of numberTwo is less than or equal the value of numberThree. Change the code so that both expressions in the console.log() statement logs true.
+
+const numberOne = 1;
+const numberTwo = 4;
+const numberThree = 2;
+console.log(numberOne <= numberTwo, numberTwo <= numberThree);
+console.log(numberOne <= numberTwo, numberTwo >= numberThree); //my answer
