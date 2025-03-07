f4e8a265af0d6b6b36577bea045bef471701b4d1
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-07 19:14:33 +0330

    quiz4

---
+++ quiz4.js
@@ -0,0 +1,11 @@
+//========================?Q-4==================================
+//? write an if statement that types "even" for an even number and "odd" for  an odd numbers  in the console.
+let number = 8;
+
+if (number>=4 ){
+    
+    console.log("even");
+} else {
+   
+    console.log("odd");
+}
