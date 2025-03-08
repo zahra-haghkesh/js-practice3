d3bf3e703ae527204b669c1708a0be1645af6137
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-08 18:42:47 +0330

    quiz1

---
+++ quiz1.js
@@ -0,0 +1,25 @@
+/**
+ * /*
+ *   Array literals
+ *   --------------
+ *   Declare some variables assigned to arrays of values
+ *   Do Not change the name of variable below
+ *
+ * @format
+ */
+
+let numbers=[1,2,3,4,5,6,7,8,9,10]; // add numbers from 1 to 10 into this array
+let mentors= ['Anahid', 'Ali', 'Mehdi']; // Create an array with the names of the mentors: Anahid, Ali and Mehdi
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+console.log(numbers);
+console.log(mentors);
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  [1,2,3,4,5,6,7,8,9,10]
+  ['Anahid', 'Ali', 'Mehdi']
+*/
