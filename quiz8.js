e809070612462ae5065cdb52e1078e6a0662f574
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-08 23:01:57 +0330

    quiz8

---
+++ quiz8.js
@@ -0,0 +1,22 @@
+//?Q-6:Read these documents and write an example for each method.
+//todo-1:https://www.programiz.com/javascript/library/array/includes
+//todo-2:https://www.programiz.com/javascript/library/array/indexof
+//todo-3:https://www.programiz.com/javascript/library/array/slice
+//todo-4:https://www.programiz.com/javascript/library/array/sort
+const people = ["mohammad", "hasti", "mahsa" , "vajiheh" , "sara" , "arian" ]
+console.log(people.includes("mahsa"));
+
+//------------------------------------------------------
+
+const numbers = [70 , 75 ,80 ,85]
+console.log(numbers.indexOf(2));
+
+//--------------------------------------------------------
+
+const person =["mina","zahra","reza","laleh","sepideh"]
+console.log(person.slice(0,1));
+
+//--------------------------------------------------------
+
+const letter =["a","t","o","s","j","z"]
+console.log(letter.sort());
