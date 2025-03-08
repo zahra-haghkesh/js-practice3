c2ca6b7941afad907d4c3611e996aee8ee3c1f7c
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-08 22:33:00 +0330

    quiz6

---
+++ quiz6.js
@@ -0,0 +1,6 @@
+//?Q-4-Consider this array. Remove the colors blue and yellow.
+// Please use variables given to you and Do Not change the name of them!!
+let colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+colors.splice(2,2);
+let filterdColors = colors
+console.log(filterdColors);
