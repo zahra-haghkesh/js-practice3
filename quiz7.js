c49ed656fce2e53383bc85a980259aa1ee44fe64
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-08 22:37:13 +0330

    quiz7

---
+++ quiz7.js
@@ -0,0 +1,7 @@
+//?Q-5:Take these two arrays and convert them into one array.
+// Please use variables given to you and Do Not change the name of them!!
+
+let primeNumbers = [2, 3, 5, 7];
+let evenNumbers = [2, 4, 6, 8];
+let oneArray = primeNumbers.concat(evenNumbers);
+console.log(oneArray );
