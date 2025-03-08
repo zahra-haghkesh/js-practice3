7c7b0af212bf9dd56078a406401c5a58413df163
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-08 19:18:50 +0330

    quiz3

---
+++ quiz3.js
@@ -0,0 +1,38 @@
+//?Q-1:Write a code, give a number from 0 to 6, and show the day of the week (solve it with switch case).
+// Please use variables given to you and Do Not change the name of them!!
+/*
+ 0=>monday
+1=>tuesday
+...
+*/
+
+let number = 5;
+let result;
+switch (number) {
+    case 0:
+    console.log("monday");
+     break;
+     case 1:
+     console.log("tuesday");
+    break;
+     case 2:
+     console.log("wednesday");
+    break;
+    case 3:
+      console.log("thursday");
+     break;
+     case 4:
+     console.log("friday");
+     break;
+     case 5:
+     console.log("saturday");
+     break;
+    case 6:
+     console.log("sunday");
+     break;
+
+                        
+
+    default:
+    console.log("march");
+}
