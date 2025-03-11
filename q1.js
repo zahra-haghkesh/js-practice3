e1d377c0e5d8793549b7a19a95af308406bb90bb
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-11 17:46:48 +0330

    q1

---
+++ q1.js
@@ -0,0 +1,34 @@
+/**
+ * /*
+ *   Array properties
+ *   ----------------
+ *   Complete the function to test if an array is empty (has no values in it)
+ *
+ * @format
+ */
+
+function isEmpty(arr) {
+  if (arr===numbers){
+    console.log(false);
+  }else {
+    console.log(true);
+  }
+  return arr; 
+
+}
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+let numbers = [1, 2, 3];
+let names = [];
+
+console.log(isEmpty(numbers));
+console.log(isEmpty(names));
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  false
+  true
+*/
