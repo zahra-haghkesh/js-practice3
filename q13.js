fa2e5f9595c0fe52c354dc9d29853b0d0664594c
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-12 22:53:57 +0330

    q13

---
+++ q13.js
@@ -0,0 +1,13 @@
+//?: write a function to calculate the square root of a number
+// hint: use Math.sqrt()
+
+function squareRootFunc(num) {
+  return Math.sqrt(num);
+}
+
+console.log(squareRootFunc(100));
+console.log(squareRootFunc(4));
+
+
+squareRootFunc(100); // 10
+squareRootFunc(4); // 2
