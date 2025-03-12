418f0afcafd842f50891a3f50152bef6d4605cbf
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-12 21:40:40 +0330

    q9

---
+++ q9.js
@@ -0,0 +1,20 @@
+/** @format */
+//add comments
+// explain what this function does. you can use Google!
+
+function whatDoIDo() {
+  return Math.random() * 8 + 1;
+}
+
+// explain what does this function do, google it
+function whatICanDo(word1, word2) {
+  return word1.concat(word2);
+}
+
+
+//----------------------------------------------------------
+
+// 1.Math.random(): static method is that's greater than or equal to 0 and less than 1 
+// this example : Number start from 8 and rise 1 by 1 .
+
+// 2. word1.concat : mixing 2 word and creating a new word.
