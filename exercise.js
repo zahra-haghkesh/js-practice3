076f9e9c27f24e894b2acb667492d70b70cb2e1a
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-16 22:15:20 +0330

    ex+ex2

---
+++ exercise.js
@@ -0,0 +1,22 @@
+/*
+  Array methods - sort
+  --------------------
+*/
+
+let numbers = [3, 2, 1];
+let sortedNumbers= numbers.sort((a ,b) => a-b); // complete this statement
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+
+console.log(sortedNumbers);
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  [1, 2, 3]
+*/
+
+//////////////
+export default sortedNumbers
---
+++ exercise2.js
@@ -0,0 +1,25 @@
+/*
+  Array methods - concat
+  ----------------------
+  The variable everyone should be an array containing both mentors and students.
+*/
+
+const mentors = ["Bahar", "Fatemeh", "Mahjan"];
+const students = ["Arad", "Pedram", "AmirMostafa", "Mohsen"];
+
+const everyone = mentors.concat(students); // complete this statement
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+
+console.log(everyone);
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  ["Bahar", "Fatemeh", "Mahjan", "Arad", "Pedram", "AmirMostafa", "Mohsen"]
+*/
+
+/////////////////
+export default everyone
